4034229fda23782a4d04b759da05fb709eb6117c
abdu0328 <abdu0328@algonquinlive.com>
2024-11-22 11:55:29 -0500

    initial App.jsx firebase/firebase structure with contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+import db from './utils/db'
+import { collection, getDocs } from 'firebase/firestore'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Contact = ({ id, firstName, lastName, email }) => {
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='contact'>
+      <h2>{`${firstName} ${lastName}`}</h2>
+      <p>{email}</p>
     </div>
+  );
+
+}
+
+function App() {
+
+  const [contacts, setContacts] = useState([]);
+
+  const fetchContacts = async () => {
+    const docsSnapshot = await getDocs(collection(db, "contacts"));
+    const data = docsSnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+  }));
+  setContacts(data);
+};
+
+console.log(contacts); // Output: {id: "1", name: "John Doe", phone: "1234567890"
+
+useEffect(() => {
+  fetchContacts();
+}, []);
+
+  return (
+    <>
+    {contacts.map((contact) => (
+      <Contact 
+        key={contact.id}
+        id={contact.id} 
+        firstName={contact.firstName} 
+        lastName={contact.lastName} 
+        email={contact.email} 
+      />
+    ))}
+    </>
   )
 }
 
---
+++ src/utils/db.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpEnZgxSzML7vc92lUjmTrnKJZ68AmJ_M",
+  authDomain: "contact-book-bc9fc.firebaseapp.com",
+  projectId: "contact-book-bc9fc",
+  storageBucket: "contact-book-bc9fc.firebasestorage.app",
+  messagingSenderId: "1071261361853",
+  appId: "1:1071261361853:web:f49e006ed6ca6c87f5b9d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
